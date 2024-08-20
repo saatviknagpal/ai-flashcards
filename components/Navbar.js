73a582958d3d66e24ecd190180cf967d61eb25e1
Saatvik <nagpal.saa@northeastern.edu>
2024-08-20 10:24:35 -0700

    added stripe integration

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,12 @@ import DarkModeToggle from "./dark-mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { Home } from "lucide-react"; // Import the Home icon from lucide-react
+import { Home } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center">
-      <div className="flex items-center space-x-4">
+      <div className="flex justify-center items-center gap-3">
         <Link href="/">
           <Home className="w-6 h-6 cursor-pointer" />
         </Link>
@@ -17,6 +17,9 @@ const Navbar = () => {
       <div className="flex items-center space-x-4">
         <Link href="/saved-flashcards">
           <Button variant="outline">Saved Flashcards</Button>
+        </Link>
+        <Link href="/pricing">
+          <Button variant="outline">Pricing</Button>
         </Link>
         <UserButton afterSignOutUrl="/" />
       </div>
